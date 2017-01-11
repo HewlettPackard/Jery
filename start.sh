4364a44d845acf1756397159f8bfcc6f286dd4d0
Marcel Jakob <marcel.jakob@hpe.com>
2017-01-11 15:09:55 +0100

    changed name of the script to jery

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ export ORACLE_HOME=/home/oracle/client11gr2
 export LD_LIBRARY_PATH=/home/oracle/client11gr2/lib:/lib:/usr/lib:/usr/openwin/lib:/usr/td/lib:/usr/ucblib:/usr/local/lib:/home/oracle/client/11gr2/lib
 export PYTHONPATH=/usr/yann/dwh2.3/lib
 
-python /usr/yann/dwh2.3/dwh2.py
+python /usr/yann/dwh2.3/jery.py
