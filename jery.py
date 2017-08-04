f39a80e51c7a2aee3e7c152fe54974d869dcbd8c
Marcel Jakob <marcel.jakob@hpe.com>
2017-08-04 12:50:59 +0200

    implemented marketwatch and datamintenance  transaction

--- jery.py
+++ jery.py
@@ -1235,7 +1235,8 @@ class LoadThread(threading.Thread):
             except cx_Oracle.DatabaseError:
                 pass
 
-            LoadThread.printDBMSoutput(self, cur)
+            #LoadThread.printDBMSoutput(self, cur)
+            print "bv"
 
             cur.close()
 
@@ -1301,7 +1302,113 @@ class LoadThread(threading.Thread):
                 END;
              """)
 
-            LoadThread.printDBMSoutput(self, cur)
+            #LoadThread.printDBMSoutput(self, cur)
+            print "cp"
+
+            cur.close()
+
+    def marketwatchTransaction(self, con):
+        if con:
+            cur = con.cursor()
+
+            cur.callproc("dbms_output.enable")
+            cur.execute("""
+                DECLARE 
+                acct_id NUMBER;
+                cust_id NUMBER;
+                ending_co_id NUMBER;
+                industry_name VARCHAR2(50);
+                starting_co_id 	NUMBER;
+                
+                marketWatchFrame1_tbl  MarketWatchFrame1_Pkg.MarketWatchFrame1_tab := MarketWatchFrame1_Pkg.MarketWatchFrame1_tab();
+                 
+                marketWatchFrame1rec MarketWatchFrame1_Pkg.MarketWatchFrame1_record ;
+                        
+                BEGIN
+                
+                select hs_ca_id into acct_id from ( select hs_ca_id, row_number() over (order by hs_ca_id) rno from holding_summary order by rno) where  rno = ( select round (dbms_random.value (1,25000)) from dual);
+                select wl_c_id  into cust_id from ( select wl_c_id, row_number() over (order by wl_c_id) rno from watch_list order by rno) where  rno = ( select round (dbms_random.value (1,5000)) from dual);
+                
+                --DEBUGGING
+                dbms_output.put_line('acct_id: ' || acct_id);
+                dbms_output.put_line('cust_id: ' || cust_id);
+                
+                marketWatchFrame1_tbl := MarketWatchFrame1_Pkg.MarketWatchFrame1(acct_id, cust_id, ending_co_id, industry_name, starting_co_id);
+                
+                END;
+             """)
+
+            #LoadThread.printDBMSoutput(self, cur)
+            print "mw"
+
+            cur.close()
+
+    def datamaintenanceTransaction(self, con):
+        if con:
+            cur = con.cursor()
+
+            cur.callproc("dbms_output.enable")
+            cur.execute("""
+                DECLARE 
+                in_acct_id NUMBER;
+                in_c_id NUMBER;
+                in_co_id NUMBER;
+                day_of_month INTEGER;
+                symbol VARCHAR2(50);
+                table_name VARCHAR2(50);
+                in_tx_id VARCHAR2(50);
+                vol_incr INTEGER;
+                status INTEGER;
+                
+                dataMaintenanceFrame1_out INTEGER;
+                
+                BEGIN 
+                select ap_ca_id into in_acct_id from ( select ap_ca_id, row_number() over (order by ap_ca_id) rno from account_permission order by rno) where  rno = ( select round (dbms_random.value (1,25000)) from dual);
+                select c_id into in_c_id from ( select c_id, row_number() over (order by c_id) rno from customer order by rno) where  rno = ( select round (dbms_random.value (1,5000)) from dual);
+                select co_id into in_co_id from ( select co_id, row_number() over (order by co_id) rno from company order by rno) where  rno = ( select round (dbms_random.value (1,2500)) from dual);
+                select round (dbms_random.value (1, 31)) into day_of_month from dual;
+                select dm_s_symb into symbol from ( select dm_s_symb, row_number() over (order by dm_s_symb) rno from daily_market order by rno) where  rno = ( select round (dbms_random.value (1,3425)) from dual);
+                with tablenames as (
+                      select 'ACCOUNT_PERMISSION' as s from dual union all
+                      select 'ADDRESS' as s from dual union all
+                      select 'COMPANY' as s from dual union all
+                      select 'CUSTOMER' as s from dual union all
+                      select 'CUSTOMER_TAXRATE' as s from dual union all
+                      select 'DAILY_MARKET' as s from dual union all
+                      select 'FINANCIAL' as s from dual union all
+                      select 'NEWS_ITEM' as s from dual union all
+                      select 'SECURITY' as s from dual union all
+                      select 'TAXRATE' as s from dual union all
+                      select 'WATCH_ITEM' as s from dual
+                     )
+                select (select s
+                        from (select s from tablenames order by dbms_random.value) s
+                        where rownum = 1
+                       )
+                into table_name       
+                from dual;
+                select tx_id into in_tx_id from ( select tx_id, row_number() over (order by tx_id) rno from taxrate order by rno) where  rno = ( select round (dbms_random.value (0,320)) from dual);
+                select dm_vol into vol_incr from ( select dm_vol, row_number() over (order by dm_vol) rno from daily_market order by rno) where  rno = ( select round (dbms_random.value (1,4469625)) from dual);
+                
+                -- DEBUGGING
+                dbms_output.put_line('in_acct_id:   ' || in_acct_id);
+                dbms_output.put_line('in_c_id:      ' || in_c_id);
+                dbms_output.put_line('in_co_id:     ' || in_co_id);
+                dbms_output.put_line('day_of_month: ' || day_of_month);
+                dbms_output.put_line('symbol:       ' || symbol);
+                dbms_output.put_line('table_name:   ' || table_name);
+                dbms_output.put_line('in_tx_id:     ' || in_tx_id);
+                dbms_output.put_line('vol_incr:     ' || vol_incr);
+                
+                dataMaintenanceFrame1_out := DataMaintenanceFrame1_Pkg.DataMaintenanceFrame1(in_acct_id, in_c_id, in_co_id, day_of_month, symbol, table_name, in_tx_id, vol_incr, status);
+                
+                dbms_output.put_line('status ' || dataMaintenanceFrame1_out);
+                
+                END;
+             """)
+
+            # LoadThread.printDBMSoutput(self, cur)
+            print "dm"
 
             cur.close()
 
@@ -1326,14 +1433,14 @@ class LoadThread(threading.Thread):
         txns = [LoadThread.brokervolumeTransaction,
                 LoadThread.customerpositionTransaction,
                 LoadThread.placeholder,
+                LoadThread.marketwatchTransaction,
                 LoadThread.placeholder,
                 LoadThread.placeholder,
                 LoadThread.placeholder,
                 LoadThread.placeholder,
                 LoadThread.placeholder,
                 LoadThread.placeholder,
-                LoadThread.placeholder,
-                LoadThread.placeholder]
+                LoadThread.datamaintenanceTransaction]
         columnNames = ["BROKERVOLUMECOUNT",
                        "CUSTOMERPOSITIONCOUNT",
                        "MARKETFEEDCOUNT",
@@ -1345,7 +1452,7 @@ class LoadThread(threading.Thread):
                        "TRADESTATUSCOUNT",
                        "TRADEUPDATECOUNT",
                        "DATAMAINTENANCECOUNT"]
-        weight = [100, 10, 5, 30, 4, 34, 12, 23, 10, 3, 24]
+        weight = [10, 10, 5, 30, 4, 34, 12, 23, 10, 3, 100]
 
         totalWeight = np.sum(weight)
         probs = np.zeros(11)
